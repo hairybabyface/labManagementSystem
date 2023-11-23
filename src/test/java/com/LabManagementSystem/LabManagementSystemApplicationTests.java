4b78174011d2d1d02781803798f6838e0e8e10b5
avblake225 <avblake225@gmail.com>
2023-11-23 16:07:30 +0000

    Created repo

---
+++ src/test/java/com/LabManagementSystem/LabManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LabManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
