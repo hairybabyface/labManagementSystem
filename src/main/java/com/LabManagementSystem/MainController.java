8b1a931698fcee31a9833be7e3c6c41578189344
avblake225 <avblake225@gmail.com>
2023-12-05 16:13:05 +0000

    Implementng MVC architecture (without Thymeleaf). In working state.

--- src/main/java/com/LabManagementSystem/MainController.java
+++ src/main/java/com/LabManagementSystem/MainController.java
@@ -1,45 +1,24 @@
 package com.LabManagementSystem;
 
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import java.util.logging.Logger;
 
 @Controller
 public class MainController {
 
-    private UserService userService;
-    private static final Logger LOGGER = Logger.getLogger(LabManagementSystemApplication.class.getName());
-
-    public MainController(UserService userService) {
-        
-        this.userService = userService;
-    }
-
-    // handler method to handle register button
+    // handlers for GET requests
+    @GetMapping("/login")
+    public String getLoginHTML(){
+        return "login";
+    } 
+    
     @GetMapping("/register")
-    public String register(){
+    public String getRegisterHTML(){
         return "register";
-    }
+    } 
 
-    // handler method to handle user registration
-    @PostMapping("/register")
-    public String registration(@ModelAttribute("user") UserDto userDto,
-                               BindingResult result,
-                               Model model){
-
-        LOGGER.info("MainController - registration"); // debug
-
-        if(result.hasErrors()){
-            model.addAttribute("user", userDto);
-            return "/register";
-        }
-
-        userService.saveUser(userDto);
-
-        return "redirect:/register?success";
-    }
+    @GetMapping("/home")
+    public String getHomeHTML(){
+        return "home";
+    }  
 }
