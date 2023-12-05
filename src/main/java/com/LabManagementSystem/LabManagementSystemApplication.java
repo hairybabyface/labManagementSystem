70fb9e78ee9e7150b4a046b897adb1c8a4b61ec9
avblake225 <avblake225@gmail.com>
2023-12-05 17:06:23 +0000

    Put CSS code in external file

--- src/main/java/com/LabManagementSystem/LabManagementSystemApplication.java
+++ src/main/java/com/LabManagementSystem/LabManagementSystemApplication.java
@@ -2,16 +2,11 @@ package com.LabManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class LabManagementSystemApplication {
 
-	private static final Logger LOGGER = Logger.getLogger(LabManagementSystemApplication.class.getName());
-
 	public static void main(String[] args) {
-
-		LOGGER.info("Running Application");
 
 		SpringApplication.run(LabManagementSystemApplication.class, args);
 	}
