8ba1e266509fcaaf1c11a0379ceca1cae1ce7045
avblake225 <avblake225@gmail.com>
2023-12-13 16:07:58 +0000

    Created Booking entity

---
+++ src/main/java/com/LabManagementSystem/Booking.java
@@ -0,0 +1,82 @@
+package com.LabManagementSystem;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Booking {
+	private Long id;
+	private String name;
+    private String equipment;
+	private String date;
+	private String time;
+	private String duration;
+
+	protected Booking() {
+	}
+
+	protected Booking(Long id, String name, String equipment, String date, String time, String duration) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.equipment = equipment;
+		this.date = date;
+		this.time = time;
+        this.duration = duration;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEquipment() {
+		return equipment;
+	}
+
+	public void setEquipment(String equipment) {
+		this.equipment = equipment;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+    public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+}
