d827e58ece5730c8e655f095257e61ce58a167fe
hairybabyface <hairybabyface@gmail.com>
2024-10-08 17:13:43 +0100

    Updated README.txt file. Now generating WAR file for running standalone application

--- README.txt.txt
+++ README.txt.txt
@@ -1,1 +1,10 @@
 How to run the application...
+
+1) Go to C:\Program Files\Apache Software Foundation\Tomcat 10.1\bin
+2) Double click on startup.bat
+3) Open web browser and go to http://localhost:8080 (or whatever port Tomcat is running on).
+   This should show the Apache Tomcat homepage
+4) Click on Manager App and log in with the following credentials:
+   Username: admin
+   Password: admin
+5) Under Applications, click on /app
--- src/main/java/com/LabManagementSystem/LabManagementSystemApplication.java
+++ src/main/java/com/LabManagementSystem/LabManagementSystemApplication.java
@@ -2,13 +2,19 @@ package com.LabManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LabManagementSystemApplication {
+public class LabManagementSystemApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(LabManagementSystemApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LabManagementSystemApplication.class);
+    }
 }
